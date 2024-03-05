47729432bd75a7d05357913b5edb41110af65cd9
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2024-03-04 23:46:43 -0500

    pritns words

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_socketio import SocketIO,emit
 from flask_cors import CORS
+from findWord import findWord
 
 app = Flask(__name__)
 app.run(debug=True)
@@ -8,3 +9,14 @@ app.run(debug=True)
 @app.route("/")
 def home():
     return render_template('home.html')
+
+
+@app.route('/find', methods=['POST'])
+def find_words():
+    # Get the data from the request
+    data = request.json
+    
+    findWord.findWord(data)
+    
+    # Return a response (for demonstration purposes)
+    return jsonify({"message": "Arrays received successfully"})
--- findWord.py
+++ findWord.py
@@ -1,6 +1,17 @@
 import os
 import json
 
+class findWord:
+
+    @staticmethod
+    def findWord(board):
+        found_words = find_words(board)
+        # print("Found words (longest first):")
+        # for word in found_words:
+        #     print(word)
+        return "ok"
+
+
 # Load dictionary files
 dictionary_path = "dictionary"
 dictionary_files = os.listdir(dictionary_path)
@@ -16,39 +27,32 @@ for file_name in dictionary_files:
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
 # Define board
-board = [
-    ['n', 'i', 'i', 'h'],
-    ['t', 'e', 'c', 'h'],
-    ['d', 'l', 's', 'g'],
-    ['a', 'l', 'o', 's']
-]
+# board = [
+#     ['n', 'i', 'i', 'h'],
+#     ['t', 'e', 'c', 'h'],
+#     ['d', 'l', 's', 'g'],
+#     ['a', 'l', 'o', 's']
+# ]
 
 def is_valid(x, y):
     return 0 <= x < 4 and 0 <= y < 4
 
-def dfs(x, y, visited, current_word, found_words):
+def dfs(x, y, visited, current_word, board):
     if not is_valid(x, y) or visited[x][y]:
         return
 
     current_word += board[x][y]
     if len(current_word) >= 3 and current_word.lower() in word_set:
-        found_words.add(current_word.lower())
+        print(current_word.lower())
 
     visited[x][y] = True
     for dx, dy in directions:
         new_x, new_y = x + dx, y + dy
-        dfs(new_x, new_y, visited, current_word, found_words)
+        dfs(new_x, new_y, visited, current_word, board)
     visited[x][y] = False
 
-def find_words():
+def find_words(board):
     visited = [[False] * 4 for _ in range(4)]
-    found_words = set()
     for i in range(4):
         for j in range(4):
-            dfs(i, j, visited, "", found_words)
-    return sorted(found_words, key=len, reverse=True)
-
-found_words = find_words()
-print("Found words (longest first):")
-for word in found_words:
-    print(word)
+            dfs(i, j, visited, "", board)
